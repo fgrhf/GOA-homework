a18633a13d658dcee7a71036f9fa8c57a949a80d
fgrhf <annagvianishvili@gmail.com>
2026-02-04 09:13:00 -0800

    commit

---
+++ day01/main.py
@@ -0,0 +1,7 @@
+#string
+print("annamaria")
+print("gvianishvili")
+#integer
+print("15")
+#float
+print("1.70")
---
+++ main.py
@@ -0,0 +1,13 @@
+print("python is a popular,easy-to-learn,and very powerful programming language,wich is used in software and web development,datascience,machine learning,and many other fields.in this course,we'll cover the basic concepts of python,as well as build real-life projects and solve different coding challenges.python for beginners requires no prior programming experience,so let's dive right in!")
+
+# print()-ტერმინალში გამოაქვს ინფორმაცია 
+
+#integer
+print(15)  #ჩემი ასაკი
+print(15)  #ჩემი მეგობრის ასაკი
+
+#string
+name="annamaria"
+surname="gvianishvili"
+age="15"
+print(name+surname+age)
