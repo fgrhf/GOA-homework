bae88e10bfd39f91c699ce3e1347124dac21c0e3
fgrhf <annagvianishvili@gmail.com>
2026-02-16 05:00:14 -0800

    comnit

---
+++ day06/homework/main.py
@@ -0,0 +1,59 @@
+#დავალებ2
+#1რიცხვით დაწყება მაგ:1name
+#2აბსურდული სახელები ან გაუგებარი
+#3გამოტოვებები ცვლადის სახელში მაგ:first name
+#4დიდი და პატარა ასოების არევა უმიზეზოდ მაგ;name,NAME,nAmE
+
+#დავალება3
+#debugging ეწოდება კიდში შეცდომების (bugs) პოვნას და მის გასწორებას.
+
+#დავალება4
+#SintaxError-კოდის წერის წესის დარღვევა.
+#TypeError-არათავსებადი მონაცემტა ტიპები(მაგ:რიცხვი+ტექსტი).
+#NameError- ცვლადის არარსებული სახელი.
+#ValueError-არასწეი მნიშვნელობა.
+#IndexError-ინდექსი არ არსებობს სიაში.
+#IndentationError-არასწორი დაშორებები.
+
+#დავალება5
+#1
+name = 'Lika'
+print(name)
+#2
+number = 5
+text = 'apples'
+result =str(number) + text
+print(result)
+
+name = "Lika"
+name2 = name + "4"
+
+first_user = "Lika"
+second_user = "Giorgi"
+print(first_user)
+
+fiest_name = 'Data'
+last_name = 'ramdom'
+
+first_user = 'Lika'
+second_user = 'Giorgi'
+print(first_user)
+
+
+#დავალება7
+city = input('enter city: ')
+district = input('enter district: ')
+building = input('enter building: ')
+floor = input('enter floor: ')
+print("your address is: ", city, district, building, floor)
+
+
+#დავალება8
+age = input("enter your age: ")
+print(f"you are{age} years old")
+
+
+#დავალება9
+Num1 = input("enter first number: ")
+Num2 = input("enter second number: ")
+print(Num1 + Num2)
