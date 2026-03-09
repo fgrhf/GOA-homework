d67a7dc95a27c94f4bc77d461504a588975a7936
fgrhf <annagvianishvili@gmail.com>
2026-03-09 05:29:19 -0700

    comnit

---
+++ day11/homework/main.py
@@ -0,0 +1,55 @@
+#დავალება1
+#comparison operations-შედარების ოპერატორები
+# >-მეტია
+print(15>13)
+print(6>8)
+#<-ნაკლებია
+print(45<167)
+print(146<233)
+#>=-მეტია ან ტოლია
+print(155>=133)
+print(288>=398)
+#<=- ნაკლებია ან ტოლია
+print(20<=165)
+print(359<=65)
+#==-უდრის
+print("ana"=="ana")
+print("tamo"=="tako")
+#!=-არუდრის
+print("mzia"!="nano")
+print("mzia"!="mzia")
+#დავალება2
+#logical operations-ლოგიკური ოპერატორები
+#and-და(მკაცრი ოპერატორი)
+#or
+#დავალება3
+my_height = 170
+user_height = int(input("enter your height :"))
+print(user_height > my_height)
+#დავალება4
+#num1 ="21"
+#num2 = 21
+#print(num1==num2)
+#იმის გამო რომ ჩვენ ვიყენებთ უდრისის ოპერატორს  რიცხვების ტიპიც უნდა ემთხვეოდეს
+#დავალება5
+my_surname = "gvianishvili"
+user_surname = input("enter your surename :")
+print(my_surname == user_surname )
+#დავალება6
+#1False
+#2True
+#3True
+#დავლება7
+temperature = int(input("enter houms temperature :"))
+print(temperature > 30)
+#დვალება8
+# ჯერ მომხმარებლისგან მივიღოთ ტემპერატუტა ცელსიუსებში
+celsius = float(input("enter rooms temperature in celsius :"))
+#ცელსიუსი უნდა გადავიყვანოთ ფატენგეიტებში
+fahrenheit = celsius*9/5+32
+print("the cooling system turned on:",fahrenheit>89.6)
+
+
+
+
+
